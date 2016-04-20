fbb11912d06baaa386240a425f3affc64071f2b4
Liam Marshall <liam@cpan.org>
2016-04-19 21:48:52 -0500

    start switching over to gl3

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <cstdlib>
 
 #include <imgui.h>
-#include <imgui_impl_glfw.h>
+#include <imgui_impl_glfw_gl3.h>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include "gui.h"
+#include "shaders.h"
 
 static void glfw_error_callback(int error, const char* description) {
     std::cout << "GLFW error (" << error << "): " << description << '\n';
@@ -22,6 +23,13 @@ int main(int argc, char* argv[]) {
         std::cerr << "could not initialize glfw\n";
         return EXIT_FAILURE;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     window = glfwCreateWindow(640, 480, "[xe][main window]", NULL, NULL);
     if (!window) {
@@ -39,7 +47,9 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    ImGui_ImplGlfw_Init(window, true);
+    ImGui_ImplGlfwGL3_Init(window, true);
+
+    GLuint vertexShader = load_shader_from_file("shaders/vertex/passthrough.vert", GL_VERTEX_SHADER);
 
     while(!glfwWindowShouldClose(window)) {
         // main game loop
@@ -50,39 +60,31 @@ int main(int argc, char* argv[]) {
         ratio = (float)width / (float)height;
 
         glViewport(0, 0, width, height);
+
+        glClearColor(0.13f, 0.13f, 0.13f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
+        //GLfloat vertices[] = {
+        //    -0.6f, -0.4f, 0.0f,
+        //    0.6f, -0.4f, 0.0f,
+        //    0.0f, 0.6f, 0.0f,
+        //};
 
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.0f, 0.0f, 0.0f, 1.0f);
+        //GLuint VBO;
+        //glGenBuffers(1, &VBO);
+        //glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
 
-        ImGui_ImplGlfw_NewFrame();
-        {
-           // ImGui::SetNextWindowPos(ImVec2(20, 20), ImGuiSetCond_FirstUseEver);
-        //ImGui::ShowTestWindow();
-        }
-
+        ImGui_ImplGlfwGL3_NewFrame();
         render_gui();
-
         ImGui::Render();
-        
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    ImGui_ImplGlfwGL3_Shutdown();
     glfwDestroyWindow(window);
     glfwTerminate();
 
