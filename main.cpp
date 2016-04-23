8b78674732eea6363d5d947573ba149e2a4afb3c
Liam Marshall <liam@cpan.org>
2016-04-22 23:43:41 -0500

    render a triangle with gl3

--- main.cpp
+++ main.cpp
@@ -50,6 +50,37 @@ int main(int argc, char* argv[]) {
     ImGui_ImplGlfwGL3_Init(window, true);
 
     GLuint vertexShader = load_shader_from_file("shaders/vertex/passthrough.vert", GL_VERTEX_SHADER);
+    GLuint fragmentShader = load_shader_from_file("shaders/fragment/helloworld.frag", GL_FRAGMENT_SHADER);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    link_shader_program(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLfloat vertices[] = {
+        -0.6f, -0.4f, 0.0f,
+        0.6f, -0.4f, 0.0f,
+        0.0f, 0.6f, 0.0f,
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    // unbind buffer, vertex array
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
 
     while(!glfwWindowShouldClose(window)) {
         // main game loop
@@ -64,17 +95,10 @@ int main(int argc, char* argv[]) {
         glClearColor(0.13f, 0.13f, 0.13f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //GLfloat vertices[] = {
-        //    -0.6f, -0.4f, 0.0f,
-        //    0.6f, -0.4f, 0.0f,
-        //    0.0f, 0.6f, 0.0f,
-        //};
-
-        //GLuint VBO;
-        //glGenBuffers(1, &VBO);
-        //glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         ImGui_ImplGlfwGL3_NewFrame();
         render_gui();
