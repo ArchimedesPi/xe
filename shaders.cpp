9e1873daae46c8267ee473d509bc822bc2be15fd
Liam Marshall <liam@cpan.org>
2016-04-22 23:43:11 -0500

    Add shader program linker

--- shaders.cpp
+++ shaders.cpp
@@ -33,3 +33,15 @@ GLuint load_shader_from_file(const char *path, GLenum shader_type) {
 
     return load_shader_from_string(shader_c_string, shader_type);
 }
+
+void link_shader_program(GLuint shader_program) {
+    glLinkProgram(shader_program);
+
+    GLint success;
+    GLchar info[512];
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shader_program, 512, NULL, info);
+        std::cerr << "shader program (" << shader_program << ") compilation failed: " << info << '\n';
+    }
+}
--- shaders.h
+++ shaders.h
@@ -4,4 +4,7 @@
 GLuint load_shader_from_string(const char *shader_string, GLenum shader_type);
 GLuint load_shader_from_file(const char *path, GLenum shader_type);
 
+// just a wrapper with error reporting
+void link_shader_program(GLuint shader_program);
+
 #endif
