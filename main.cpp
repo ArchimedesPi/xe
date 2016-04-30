a5d46d622a3d262ed49584f7b9286b049a4fd697
Liam Marshall <liam@cpan.org>
2016-04-30 13:59:47 -0500

    Render rectangle (uses an EBO)

--- main.cpp
+++ main.cpp
@@ -60,19 +60,28 @@ int main(int argc, char* argv[]) {
     glDeleteShader(fragmentShader);
 
     GLfloat vertices[] = {
-        -0.6f, -0.4f, 0.0f,
-        0.6f, -0.4f, 0.0f,
-        0.0f, 0.6f, 0.0f,
+         0.5f,  0.5f, 0.0f,  // Top Right
+         0.5f, -0.5f, 0.0f,  // Bottom Right
+        -0.5f, -0.5f, 0.0f,  // Bottom Left
+        -0.5f,  0.5f, 0.0f   // Top Left 
+    };
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
     };
 
-    GLuint VBO, VAO;
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
@@ -97,7 +106,8 @@ int main(int argc, char* argv[]) {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         ImGui_ImplGlfwGL3_NewFrame();
