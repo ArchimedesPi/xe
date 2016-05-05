6e708d5a70844a289c598199e950a95d33eedc2a
Liam Marshall <liam@cpan.org>
2016-05-05 16:50:38 -0500

    ugh i'm not breaking this commit up

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 #include <imgui.h>
 #include <imgui_impl_glfw_gl3.h>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <SOIL.h>
 
 #include "gui.h"
 #include "shaders.h"
@@ -49,49 +52,9 @@ int main(int argc, char* argv[]) {
 
     ImGui_ImplGlfwGL3_Init(window, true);
 
-    Shader vertexShader = Shader::fromFile("shaders/vertex/passthrough.vert", GL_VERTEX_SHADER);
-    Shader fragmentShader = Shader::fromFile("shaders/fragment/helloworld.frag", GL_FRAGMENT_SHADER);
-    ShaderProgram shaderProgram = ShaderProgram::ShaderProgram()
-        .addShader(vertexShader).addShader(fragmentShader)
-        .link();
-
-    GLfloat vertices[] = {
-         0.5f,  0.5f, 0.0f,  // Top Right
-         0.5f, -0.5f, 0.0f,  // Bottom Right
-        -0.5f, -0.5f, 0.0f,  // Bottom Left
-        -0.5f,  0.5f, 0.0f   // Top Left 
-    };
-    GLuint indices[] = {
-        0, 1, 3,
-        1, 2, 3
-    };
-
-    GLuint VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-
-    // unbind buffer, vertex array
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-
     while(!glfwWindowShouldClose(window)) {
         // main game loop
-        
-        int width, height;
-        float ratio;
+        int width, height; float ratio;
         glfwGetFramebufferSize(window, &width, &height);
         ratio = (float)width / (float)height;
 
@@ -100,16 +63,9 @@ int main(int argc, char* argv[]) {
         glClearColor(0.13f, 0.13f, 0.13f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        shaderProgram.use();
-
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
         if (gui_state.render_wireframe) glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // render our stuff here
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-        glBindVertexArray(0);
 
         ImGui_ImplGlfwGL3_NewFrame();
         render_gui();
--- shaders/fragment/helloworld.frag
+++ shaders/fragment/helloworld.frag
@@ -1,7 +1,14 @@
 #version 330 core
 
+in vec3 ourColor;
+in vec2 texCoord;
+
 out vec4 color;
 
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 void main() {
-    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    //color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    color = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
 }
