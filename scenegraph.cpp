769f05b189c1f2289a52b201cbfd0f06d7e59fcb
Liam Marshall <liam@cpan.org>
2016-05-05 15:53:51 -0500

    Add a scenegraph

---
+++ scenegraph.cpp
@@ -0,0 +1,33 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "scenegraph.h"
+
+Node::Node() {}
+
+virtual Node::Node~() {
+    std::for_each(children.begin(),
+            children.end(),
+            std::mem_fun(&Node::~Node));
+    children.clear();
+}
+
+void Node::addChild(Node *child) {
+    children.push_back(child);
+}
+
+void Node::computeLocalTransforms() {
+    local_matrix = glm::translate(local_matrix, glm::vec3(x,y,z));
+    local_matrix *= glm::gtx::euler_angles(yaw, pitch, roll);
+    world_matrix = local_matrix * parent->world_matrix;
+}
+
+void Node:computeAllTransforms() {
+    computeLocalTransforms();
+
+    std::for_each(children.begin(),
+            children.end(),
+            std::mem_fun(&Node::computeLocalTransforms));
+}
---
+++ scenegraph.h
@@ -0,0 +1,24 @@
+#ifndef __SCENEGRAPH_H__
+#define __SCENEGRAPH_H__
+
+class Node {
+public:
+    Node();
+    virtual Node~();
+
+    void addChild(Node *child);
+
+    void computeLocalTransforms();
+    void computeAllTransforms();
+
+    glm::mat4 local_matrix;
+    glm::mat4 world_matrix;
+
+    float x=0, y=0, z=0;
+    float roll=0, pitch=0, yaw=0;
+
+    Node *parent;
+    std::list<Node*> children;
+};
+
+#endif
