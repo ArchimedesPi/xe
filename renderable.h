c04e7ea8715ce2bfe37905225924cd8069901e21
Liam Marshall <liam@cpan.org>
2016-05-07 15:44:14 -0500

    Fill in Renderable interface

--- renderable.h
+++ renderable.h
@@ -1,7 +1,14 @@
 #ifndef __RENDERABLE_H__
 #define __RENDERABLE_H__
 
+#include "gameobject.h"
+
 class Renderable {
+public:
+    virtual ~Renderable();
+
+    virtual void shaderSetup();
+    virtual void render(GameObject *obj);
 };
 
 #endif
