b04f268259c3572f221f4bdad37488d3880841c5
Liam Marshall <liam@cpan.org>
2016-05-08 14:06:16 -0500

    Start implementing Renderer

---
+++ renderer.cpp
@@ -0,0 +1,24 @@
+#include "renderer.h"
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+void Renderer::addRenderable(Renderable *renderable) {
+    this->renderables.push_back(renderable);
+}
+
+void Renderer::setupRenderables() {
+    std::for_each(renderables.begin(), renderables.end(), std::mem_fn(&Renderable::setup));
+}
+
+void Renderer::renderInstances(std::vector<GameObject*> instances) {
+}
+
+void Renderer::renderInstance(GameObject *instance) {
+    // how will we attach a renderer to an instance?
+}
+
+void Renderer::render(Renderable *renderable, GameObject *instance) {
+    renderable->renderInstance(instance);
+}
