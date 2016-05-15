5de25385d112fb83e2a5cbe30fb556c614e1bb7a
Liam Marshall <liam@cpan.org>
2016-05-15 17:52:39 -0500

    Make GameObjects store a SceneNode

---
+++ gameobject.cpp
@@ -0,0 +1,5 @@
+#include "gameobject.h"
+
+void GameObject::setPosition(float x, float y, float z) {
+    n.x=x; n.y=y; n.z=z;
+}
--- gameobject.h
+++ gameobject.h
@@ -3,7 +3,11 @@
 
 #include "scenegraph.h"
 
-class GameObject : public SceneNode {
+class GameObject {
+public:
+    SceneNode n; // this stores transform data :)
+
+    void setPosition(float x, float y, float z);
 };
 
 #endif
