2e5ab7534627c67da72e44735c8070723e7f4dcc
Liam Marshall <liam@cpan.org>
2016-05-15 17:53:27 -0500

    Pass the camera through Renderables

--- renderable.h
+++ renderable.h
@@ -3,6 +3,7 @@
 
 #include "gameobject.h"
 #include "game.h"
+#include "camera.h"
 
 class Renderable {
 public:
@@ -10,7 +11,7 @@ public:
 
     virtual void setup() = 0;
 
-    virtual void renderInstance(GameObject *obj, Game *game) = 0;
+    virtual void renderInstance(GameObject *obj, Game *game, Camera *camera) = 0;
 };
 
 #endif
