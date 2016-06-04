1af81c2cb8aee910ef0c3d8296a51f1a808f8e96
Liam Marshall <liam@cpan.org>
2016-06-04 15:51:54 -0500

    immediate shading and stuff

--- cube.cpp
+++ cube.cpp
@@ -2,6 +2,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #include "cube.h"
@@ -38,19 +39,24 @@ void CubeRenderer::renderInstance(GameObject *obj, Game *game, Camera *camera) {
     shader.use();
 
     glm::mat4 model, view, projection;
+    glm::mat3 normalmat;
    
     model = obj->n.world_matrix;
 
     view = camera->lookMatrix();
     projection = glm::perspective(45.0f, game->ratio, 0.1f, 100.0f);
 
+    normalmat = glm::inverseTranspose(glm::mat3(model));
+
     glUniformMatrix4fv(shader.uniform("model"), 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(shader.uniform("view"), 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(shader.uniform("projection"), 1, GL_FALSE, glm::value_ptr(projection));
+    glUniformMatrix3fv(shader.uniform("normalmat"), 1, GL_FALSE, glm::value_ptr(normalmat));
 
     glUniform4f(shader.uniform("object_color"), 1.0f, 0.5f, 0.2f, 1.0f);
     glUniform3f(shader.uniform("light_color"), 1.0f, 1.0f, 1.0f);
     glUniform3f(shader.uniform("light_position"), camera->eye.x, camera->eye.y, camera->eye.z);
+    glUniform3f(shader.uniform("view_position"), camera->eye.x, camera->eye.y, camera->eye.z);
 
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 36);
--- shaders/fragment/simplelighting.frag
+++ shaders/fragment/simplelighting.frag
@@ -14,13 +14,13 @@ void main() {
     vec3 norm = normalize(normal);
     vec3 light_direction = normalize(light_position - fragment_position);
     vec3 view_direction = normalize(view_position - fragment_position);
-    vec3 reflect_direction = reflect(-light_direction, norm);
+    vec3 reflect_direction = reflect(light_direction, norm);
+
+    float specular_strength = 1.0f;
 
     vec3 ambient = light_color * 0.1;
-    float specular_intensity = pow(max(dot(view_direction, reflect_direction), 0.0), 64);
-    vec3 specular = 1.0f * specular_intensity * light_color;
     vec3 diffuse = max(dot(norm, light_direction), 0.0f) * light_color;
 
-    color = vec4((ambient + diffuse + specular) * object_color.xyz, object_color.z);
+    color = vec4((ambient + diffuse) * object_color.xyz, object_color.z);
 }
 
