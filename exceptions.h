6419f36427bc5d695dc39081ee7502ba275dc46a
Liam Marshall <liam@cpan.org>
2016-06-22 11:35:14 -0500

    Fix exceptions.h; it was misdumping exceptions

--- exceptions.h
+++ exceptions.h
@@ -1,26 +1,17 @@
 #ifndef __XE_EXCEPTIONS__
 #define __XE_EXCEPTIONS__
 #include <exception>
-using namespace std;
 
-class ShaderCompileException : public exception {
-private:
-    string message_;
+class ShaderCompileException : public std::runtime_error {
 public:
-    explicit ShaderCompileException(const std::string& message) : message_(message) {}
-    virtual const char* what() const throw() {
-        return ("Error compiling shader: " + message_).c_str();
-    }
+    explicit ShaderCompileException(const std::string& message)
+        : std::runtime_error("Error compiling shader: " + message) {}
 };
 
-class ShaderLinkException : public exception {
-private:
-    string message_;
+class ShaderLinkException : public std::runtime_error {
 public:
-    explicit ShaderLinkException(const std::string& message) : message_(message) {}
-    virtual const char* what() const throw() {
-        return ("Error linking shaders: " + message_).c_str();
-    }
+    explicit ShaderLinkException(const std::string& message)
+        : std::runtime_error("Error linking shaders: " + message) {}
 };
 
 #endif
