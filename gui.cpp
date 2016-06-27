b89564d15f4478f1d21e431aecd3d155dd095c4b
Liam Marshall <liam@cpan.org>
2016-06-26 22:54:35 -0500

    Add a log window in the GUI

--- gui.cpp
+++ gui.cpp
@@ -1,26 +1,42 @@
-#include <imgui.h>
 #include "gui.h"
+#include <cstdlib>
 
 struct GUIState gui_state = {
+    .imgui_test_window_open = false,
+
     .imgui_metrics_window_open = false,
     .xe_metrics_window_open = false,
 
+    .log_window_open = false,
+
     .render_mnormals = false,
     .render_datastructures = false,
-    .render_wireframe = false,
+    .render_wireframe = true,
 };
 
+
 void render_gui() {
+    static LogWindow log_window;
+
     render_main_menubar();
+
+    // demo
+    if (gui_state.imgui_test_window_open) ImGui::ShowTestWindow(&gui_state.imgui_test_window_open);
+
+    // metrics
     if (gui_state.imgui_metrics_window_open) ImGui::ShowMetricsWindow(&gui_state.imgui_metrics_window_open);
     if (gui_state.xe_metrics_window_open) {}
+
+    // commands and logs
+    if (gui_state.log_window_open) log_window.render("[xe][log]", &gui_state.log_window_open);
 }
 
 static void render_main_menubar() {
     if (ImGui::BeginMainMenuBar()) {
-        if (ImGui::BeginMenu("View")) {
+        if (ImGui::BeginMenu("File"))
+            render_mmb_file();
+        if (ImGui::BeginMenu("View"))
             render_mmb_view();
-        }
         ImGui::EndMainMenuBar();
     }
 }
@@ -32,8 +48,44 @@ static void render_mmb_view() {
         ImGui::MenuItem("Render as wireframes", NULL, &gui_state.render_wireframe);
         ImGui::EndMenu();
     }
+    ImGui::MenuItem("IMGUI Test", NULL, &gui_state.imgui_test_window_open);
     ImGui::MenuItem("IMGUI Metrics", NULL, &gui_state.imgui_metrics_window_open);
     ImGui::MenuItem("xe metrics", NULL, &gui_state.xe_metrics_window_open);
+    ImGui::MenuItem("Log", NULL, &gui_state.log_window_open);
 
     ImGui::EndMenu();
+}
+
+static void render_mmb_file() {
+    if (ImGui::MenuItem("Quit", "Ctrl+Q")) {
+        std::exit(0);
+    }
+    ImGui::EndMenu();
+}
+
+
+void LogWindow::render(const char *title, bool *opened) {
+    ImGui::SetNextWindowSize(ImVec2(500,400), ImGuiSetCond_FirstUseEver);
+    ImGui::Begin(title, opened);
+ 
+    if (ImGui::Button("Clear")) this->clear();
+    ImGui::SameLine();
+    bool copy_log = ImGui::Button("Copy");
+
+    ImGui::Separator();
+
+    ImGui::BeginChild("scrolling");
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 1));
+    if (copy_log) ImGui::LogToClipboard();
+    ImGui::TextUnformatted(buffer.begin());
+    if (scrolldown)
+        ImGui::SetScrollHere(1.0f);
+    scrolldown = false;
+    ImGui::PopStyleVar();
+    ImGui::EndChild();
+    ImGui::End();
+}
+
+void LogWindow::clear() {
+    buffer.clear();
 }
--- gui.h
+++ gui.h
@@ -1,18 +1,36 @@
 #ifndef __GUI_H__
 #define __GUI_H__
 
+#include <imgui.h>
+
 static void render_main_menubar();
 static void render_mmb_view();
+static void render_mmb_file();
 
 struct GUIState {
+    bool imgui_test_window_open;
+
     bool imgui_metrics_window_open;
     bool xe_metrics_window_open;
+
+    bool log_window_open;
 
     bool render_mnormals;
     bool render_datastructures;
     bool render_wireframe;
 };
 extern struct GUIState gui_state;
+
+class LogWindow {
+public:
+    LogWindow() {}
+    void render(const char *title, bool *opened);
+    void clear();
+
+private:
+    ImGuiTextBuffer buffer;
+    bool scrolldown;
+};
 
 void render_gui();
 
